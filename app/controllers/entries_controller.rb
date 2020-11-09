310cc7150692b8173fa490555b5a07acc41aa580
candacecodes <candacehchoe@gmail.com>
2020-11-08 20:30:06 -0800

    updated create for submit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,16 +5,15 @@ class EntriesController < ApplicationController
     end
 
     def create 
-        # entry = Entry.new(entry_params) 
-        entry = Entry.new
+        entry = Entry.new(entry_params) 
         entry.save 
         render json: entry
     end 
 
-    # private 
-    # def card_params
-    #     params.require(:user_id).permit(:date, :title, :context, :intensity_level, :mood_id)
-    # end 
+    private 
+    def entry_params
+        params.permit(:user_id, :date, :title, :context, :intensity_level, :mood_id)
+    end 
 end
 
 # Entry.create(date: "date1", title: "entrytitle1", context: "entrycontent1", intensity_level: "1", mood_id:1, user_id: 1)
